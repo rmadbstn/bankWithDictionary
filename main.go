03eebe5403d078817677ff35ad090e3373aed728
Harvey <kumyunsu@gmail.com>
2024-06-14 17:56:02 +0900

    2024/06/14 goStudy

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"name/mydict"
+)
 
+func main() {
+	// account := accounts.NewAccount("nico")
+	// account.Deposit(10)
+	// fmt.Println(account)
+
+	// dictionary := mydict.Dictionary{"first": "First word"}
+	// word := "hello"
+	// definition := "Greeting"
+	// err := dictionary.Add(word, definition)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// hello, _ := dictionary.Search(word)
+	// fmt.Println("found", word, "defintion:", hello)
+	// err2 := dictionary.Add(word, definition)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "First")
+	// err := dictionary.Update("sdsd", "Second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search(baseWord)
+	// fmt.Println(word)
+
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(word)
 }
